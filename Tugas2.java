e6939895ddc73682d9ff4781507ae18e82778d61
nurfadhilah55 <39688096+nurfadhilah55@users.noreply.github.com>
2018-05-27 22:19:22 -0700

    Add files via upload

---
+++ Tugas2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas2;
+
+/**
+ *
+ * @author danil
+ */
+public class Tugas2 {
+    public static void main(String[] args) {
+        System.out.println("Tugas struktur data membuat perkalian 8 menggunakan looping");
+        System.out.println("Dengan for");
+        for (int i=1;i<11;i++){
+            int hslfor = i*8;
+            System.out.println(i + " x 8 = " + hslfor);
+        }
+    }
+    
+}
